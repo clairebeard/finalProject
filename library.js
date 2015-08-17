216ac4c9ea763ab5093993263da72c7756eecbc0
sarahbsten89 <sarahbsten@gmail.com>
2015-08-17 14:19:50 -0700

    kfsajlksfalk

--- library.js
+++ library.js
@@ -1,32 +1,57 @@
-var ref = new Firebase("https://vivid-fire-368.firebaseio.com/");
+var data = new Firebase("https://vivid-fire-368.firebaseio.com/");
+var authData = data.getAuth();
     //var usersRef = ref.child("users");
-    
+
+function saveFunction(){
+/*ref.authWithOAuthPopup("google", function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+});
+	function authDataCallback(authData){
+		if (authData){
+			console.log("User" + authData.uid + " is logged in with " + authData.provider);
+		} else {
+			console.log("User is logged out");
+		}
+	}
+*/
 var pic = document.getElementById('file').value;
 var stories = document.getElementById('story').value;
 var dates = document.getElementById('date').value;
-console.log(dates);
-console.log(stories);
-
-function convertImage(url, callback, outputFormat){
-    var img = document.getElementById('file').value;;
-    img.crossOrigin = 'Anonymous';
-    img.onload = function(){
-        var canvas = document.createElement('CANVAS'),
-        ctx = canvas.getContext('2d'), dataURL;
-        canvas.height = this.height;
-        canvas.width = this.width;
-        ctx.drawImage(this, 0, 0);
-        dataURL = canvas.toDataURL(outputFormat);
-        callback(dataURL);
-        canvas = null; 
-    };
-    img.src = url;
+	authData.uid.push({
+		'username': { 
+				'authData.uid':{
+					'photo':{
+						'file':{
+							'pic': file,
+							'description': stories,
+							'date': dates
+					}
+				}
+			}
+		}	
+	})
 }
 
-function saveFunction(){
-ref.set({
+
+// Generate a reference to a new location and add some data using push()
+
+
+/*
+	ref.set({
+      Sarah: {
+        picture: pic,
+        	description: stories,
+        	date: dates
+      }
+    });
+}*/
+//in the beginning, when we start the thing the data needs to be ref.set({
 /*var i = 0;*/
-	"0":{
+	/*"0":{
 		'username': { 
 				"Sarah":{
 					'photo':{
@@ -39,15 +64,4 @@ ref.set({
 			}
 		}	
 	}
-})
-}
-
-/*
-	ref.set({
-      Sarah: {
-        picture: pic,
-        	description: stories,
-        	date: dates
-      }
-    });
-}*/
+})*/
