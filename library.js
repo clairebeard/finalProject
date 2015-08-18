cc93b5e59ce2f0def5c2de2914686711546475d0
clairebeard <beard.claire@hotmail.com>
2015-08-18 10:23:00 -0700

    loading pictures to the database

--- library.js
+++ library.js
@@ -1,67 +1,38 @@
 var data = new Firebase("https://vivid-fire-368.firebaseio.com/");
-/*var authData = data.getAuth();*/
-    //var usersRef = ref.child("users");
 
 function saveFunction(){
-/*ref.authWithOAuthPopup("google", function(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-  }
-});
-	function authDataCallback(authData){
-		if (authData){
-			console.log("User" + authData.uid + " is logged in with " + authData.provider);
-		} else {
-			console.log("User is logged out");
-		}
-	}
-*/
-var pic = document.getElementById('file').value;
-var stories = document.getElementById('story').value;
-var dates = document.getElementById('date').value;
-	data.push({
-		'username': { 
-				'Sarah':{
-					'photo':{
-						'file':{
-							'pic': file,
-							'description': stories,
-							'date': dates
+	var newImage = document.getElementById('previewing').src;
+	console.log(newImage);
+
+	var pic = document.getElementById('file').value;
+	var stories = document.getElementById('story').value;
+	var dates = document.getElementById('date').value;
+		data.push({
+			'username': { 
+					'Sarah':{
+						'photo':{
+							'file':{
+								'pic': newImage,
+								'description': stories,
+								'date': dates
+						}
 					}
 				}
-			}
-		}	
-	})
+			}	
+		})
+}
+
+function loadPics(){
+	data.on("value", function(snapshot) {
+	  console.log(snapshot.val());
+	}, 
+	/*for (var = 0; i<data.length */ 
+	function (errorObject) {
+	  console.log("The read failed: " + errorObject.code);
+	});	
+	/*data.child('photo').display;
+	console.log('yay it worked');*/
 }
 
 
 // Generate a reference to a new location and add some data using push()
-
-
-/*
-	ref.set({
-      Sarah: {
-        picture: pic,
-        	description: stories,
-        	date: dates
-      }
-    });
-}*/
-//in the beginning, when we start the thing the data needs to be ref.set({
-/*var i = 0;*/
-	/*"0":{
-		'username': { 
-				"Sarah":{
-					'photo':{
-						'file':{
-							'pic': file,
-							'description': stories,
-							'date': dates
-					}
-				}
-			}
-		}	
-	}
-})*/
